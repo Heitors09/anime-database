de1967492451c364be4161b601655bb287094c4d
Heitors09 <heitorao32@gmail.com>
2025-06-14 23:23:40 +0000

    fix: fix constants images

--- src/app/constants/data.ts
+++ src/app/constants/data.ts
@@ -24,7 +24,7 @@ export const popularBanner = [
     id: 1,
     name: 'Shingeki no Kyojin',
     description: 'In a world where humanity lives inside cities surrounded by enormous walls to protect themselves from giant creatures that devour humans, Eren Yeager swears revenge after witnessing his mother\'s death and his city\'s destruction by one of these titans.',
-    banner: '/mock-images/attack-on-titan.jpg',
+    banner: 'https://cdn.myanimelist.net/images/anime/10/47347.jpg',
     streaming: {
       platforms: ['Crunchyroll', 'Funimation', 'Netflix'],
     },
@@ -33,7 +33,7 @@ export const popularBanner = [
     id: 2,
     name: 'Hunter x Hunter',
     description: 'Gon Freecss, a 12-year-old boy, discovers that his father, who he thought was dead, is actually one of the world\'s best Hunters. Determined to follow in his father\'s footsteps, Gon embarks on a journey to become a Hunter and find his father.',
-    banner: '/mock-images/hunter-x-hunter.jpg',
+    banner: 'https://cdn.myanimelist.net/images/anime/11/48936.jpg',
     streaming: {
       platforms: ['Crunchyroll', 'Netflix', 'HBO Max'],
     },
@@ -42,7 +42,7 @@ export const popularBanner = [
     id: 3,
     name: 'Death Note',
     description: 'Light Yagami, a brilliant student, finds a supernatural notebook called Death Note. Upon discovering he can kill anyone whose name he writes in the notebook, Light decides to use his power to create a world free of criminals, but this attracts the attention of a genius detective known as L.',
-    banner: '/mock-images/death-note.jpg',
+    banner: 'https://cdn.myanimelist.net/images/anime/9/9453.jpg',
     streaming: {
       platforms: ['Netflix', 'Crunchyroll', 'Hulu'],
     },
@@ -51,7 +51,7 @@ export const popularBanner = [
     id: 4,
     name: 'Evangelion',
     description: 'In 2015, humanity stands on the brink of extinction after a catastrophic event known as Second Impact. Shinji Ikari is recruited by his father to pilot a giant robot called Evangelion and protect humanity from mysterious beings known as Angels.',
-    banner: '/mock-images/evangelion.jpg',
+    banner: 'https://cdn.myanimelist.net/images/anime/12/76049.jpg',
     streaming: {
       platforms: ['Netflix', 'Amazon Prime Video'],
     },
@@ -60,7 +60,7 @@ export const popularBanner = [
     id: 5,
     name: 'Sousou no Frieren',
     description: 'Frieren is an immortal elf who, after defeating the Demon King with her group of adventurers, decides to travel the world to better understand humanity. Accompanied by her apprentice Fern, she embarks on a journey of self-discovery and learning about the complexities of life and death.',
-    banner: '/mock-images/frieren.jpg',
+    banner: 'https://cdn.myanimelist.net/images/anime/1015/138022.jpg',
     streaming: {
       platforms: ['Crunchyroll', 'Netflix'],
     },
